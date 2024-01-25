41d7e68940b065857e2d16f778fedbb16a377407
specialman12 <112378723+specialman12@users.noreply.github.com>
2024-01-25 02:57:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+
+  document.body.style.setProperty("--time-zone", "UTC+2");
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+
+
